6e362d05107d46e6b7fdfe90a5b8adb021384772
John <johnmolijn@gmail.com>
2023-10-29 07:27:20 +0100

    Combined creation and upload in one file

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # ePaperImageGenerator
-generates images for the 2.9 inch epaper displays
+generates QR code image for the 2.9 inch epaper displays and uploads it to display
 
 
 
-Usage python .\createQRimagefordisplay.py  'first line' 'second line' 'third line' 'WIFI:T:WPA2;S:Guestnetwork;P:wifipassword;H:true;'
+Usage:
+python .\createQRimagefordisplay.py  'first line' 'second line' 'third line' 'WIFI:T:WPA2;S:Guestnetwork;P:wifipassword;H:true;' 'MAC (without colons)' '192.168.178.89'
 
 This creates 
 
 ![hello](https://github.com/J-o-h-n-M/ePaperImageGenerator/assets/8143724/af83f9b0-2661-4547-942d-09f608ebe686)
+
+and uploads it to the display with MAC using the access point at 192.168.178.89
--- createQRimagefordisplay.py
+++ createQRimagefordisplay.py
@@ -1,6 +1,7 @@
 import argparse
 from PIL import Image, ImageDraw, ImageFont
 import qrcode
+import requests
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description='Create a JPEG image with text and QR code')
@@ -8,8 +9,15 @@ parser.add_argument('line1', type=str, help='the first line to add to the image'
 parser.add_argument('line2', type=str, help='the second to add to the image')
 parser.add_argument('line3', type=str, help='the third to add to the image')
 parser.add_argument('qr_data', type=str, help='the data to encode in the QR code')
+parser.add_argument('mac', type=str, help='MAC of the display')
+parser.add_argument('AP_IP', type=str, help='IP addressof the AP')
 args = parser.parse_args()
 
+
+
+mac = args.mac   # destination mac address
+dither = 0   # set dither to 1 is you're sending photos etc
+apip = args.AP_IP   # ip address of your access point
 
 # Create a new image with white background
 img = Image.new('RGB', (296, 128), color='white')
@@ -44,3 +52,26 @@ img.paste(qr_img, (qr_pos_x, qr_pos_y))
 
 # Save the image as a JPEG file
 img.save('QRcode.jpg')
+
+
+
+
+
+
+
+# Save the image as JPEG with maximum quality
+image_path = 'QRcode.jpg'
+
+# Prepare the HTTP POST request
+url = "http://" + apip + "/imgupload"
+payload = {"dither": dither, "mac": mac}  # Additional POST parameter
+files = {"file": open(image_path, "rb")}  # File to be uploaded
+
+# Send the HTTP POST request
+response = requests.post(url, data=payload, files=files)
+
+# Check the response status
+if response.status_code == 200:
+    print("Image uploaded successfully!")
+else:
+    print("Failed to upload the image.")
